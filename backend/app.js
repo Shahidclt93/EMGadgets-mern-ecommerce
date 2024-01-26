b2246392d8241947952ab52d410c9c7d7e685372
Syed Shameem <shahidclt93@gmail.com>
2024-01-26 20:37:39 +0530

    frontend update

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const dotenv = require("dotenv");
 const errorMiddleware = require("./middleware/error");
 const cors = require("cors");
 
