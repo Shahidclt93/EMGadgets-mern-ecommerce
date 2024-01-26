67909660c26d82bcd0565a394699677c9067d506
Syed Shameem <shahidclt93@gmail.com>
2024-01-26 15:35:45 +0530

    update store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,8 @@ import {
   createStore,
   combineReducers,
   applyMiddleware,
-} from "@reduxjs/toolkit";
+} from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
 import {
