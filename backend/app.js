153398f6461ce987f4f270fcd4f15510b9525210
Syed Shameem <shahidclt93@gmail.com>
2024-02-08 02:44:33 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,16 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const errorMiddleware = require("./middleware/error");
-const cors = require("cors");
+const cors = require("cors");s
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-app.use(cors({ credentials: true }));
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://emgadgets-estore.netlify.app'],
+  credentials: true 
+  }));
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "config.env" });
