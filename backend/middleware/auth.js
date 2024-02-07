c68793f2aa4f019ae2588eec9f15a150b4f8214f
Syed Shameem <shahidclt93@gmail.com>
2024-02-08 01:24:36 +0530

    update

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,17 +4,12 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-  let token;
-
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  )
+  const { token } = req.cookies;
 
   if (!token) {
     return next(new ErrorHander("Please Login to access this resource", 401));
   }
-  token = req.headers.authorization.split(" ")[1];
+
   const decodedData = jwt.verify(token, process.env.JWT_SECRET);
   req.user = await User.findById(decodedData.id);
 
